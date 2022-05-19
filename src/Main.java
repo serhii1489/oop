1eeb299e7ad1d0466a1a9a9a1e588efafdb45fcb
serhii1489 <goncharenko1489@gmail.com>
2022-05-19 17:22:02 +0300

    added little string

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     doctor.speak();
     doctor.showInfo();
     doctor.ask();
+        System.out.println("------------------------------------");
     Patient patient = new Patient("Sten", "Smith", 38);
     patient.setClinical_diagnosis("appendix");
     patient.showInfo();
