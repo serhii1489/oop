607748cb637cb64c291fe6e41bab7272c67037bf
serhii1489 <goncharenko1489@gmail.com>
2022-05-26 13:20:47 +0300

    homework task with calculator

--- src/hw2_calculator/Main.java
+++ src/hw2_calculator/Main.java
@@ -5,7 +5,9 @@ import hw2_calculator.second.MyCalculator;
 public class Main {
     public static void main(String[] args) {
         MyCalculator myCalculator = new MyCalculator();
+        System.out.println("write number a:");
        int a = myCalculator.a;
+        System.out.println("write number b:");
        int b = myCalculator.b;
        myCalculator.divide();
        myCalculator.plus();
